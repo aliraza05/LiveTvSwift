da7953a135d3175fdcceb1d2119d620083b83806
Ali  Raza <>
2018-05-30 23:39:49 +0500

    see previous msg. same like that

---
+++ liveTvSwift/Pods/Target Support Files/Pods-LiveSportsTVHD/Pods-LiveSportsTVHD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiveSportsTVHD : NSObject
+@end
+@implementation PodsDummy_Pods_LiveSportsTVHD
+@end
--- liveTvSwift/Pods/Target Support Files/Pods-liveTvSwift/Pods-liveTvSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_liveTvSwift : NSObject
-@end
-@implementation PodsDummy_Pods_liveTvSwift
-@end
